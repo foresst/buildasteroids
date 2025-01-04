942e58cf9f5dd0c404899db89e15e7a4db3afc84
foresst <soriciradu@gmail.com>
2025-01-04 23:01:33 +0200

    setting up player

--- main.py
+++ main.py
@@ -11,16 +11,39 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # player2 = Player(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3)
+
+    # updatable.add(player)
+    # drawable.add(player)
+    # updatable.add(player2)
+    # drawable.add(player2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.update()
+        # set framrate to 60 FPS
         dt = clock.tick(60) / 1000
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        # redraw the screen
+        pygame.display.update()
+
+        
 
 if __name__ == "__main__":
     main()
